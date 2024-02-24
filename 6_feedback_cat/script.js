d83ac4acd19c17d2fd024d06e2036508691f7f19
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2024-02-24 23:45:04 +0700

    design UI for 6_feedback_cat

---
+++ 6_feedback_cat/script.js
@@ -0,0 +1,4 @@
+const ratings = document.querySelectorAll('.rating');
+const feedback = document.querySelector('.feedback-container');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#feedback');
