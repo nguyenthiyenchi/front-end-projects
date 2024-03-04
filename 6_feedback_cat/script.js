2ac610e93372ba9013e3bbd4557dc461e6b151dd
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2024-03-04 23:21:41 +0700

    set active for selected rating

--- 6_feedback_cat/script.js
+++ 6_feedback_cat/script.js
@@ -1,4 +1,26 @@
-const ratings = document.querySelectorAll('.rating');
-const feedback = document.querySelector('.feedback-container');
-const sendBtn = document.querySelector('#send');
-const panel = document.querySelector('#feedback');
+const ratings = document.querySelectorAll(".rating");
+const feedback = document.querySelector(".feedback-container");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#feedback");
+
+let selectedRating = null;
+
+function removeActive() {
+    for (let i = 0; i < ratings.length; i++)    {
+        ratings[i].classList.remove("active");
+    }
+}
+
+// feedback.addEventListener("click", (e) => {
+//     if (e.target.parentNode.classList.contains("rating") && e.target.nextElementSibling)
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    ratings.forEach(rating => {
+        rating.addEventListener("click", function() {
+            ratings.forEach(o => o.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
