e0dbdcd8d59f3809336b5f69529640e68e6b679c
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2024-03-05 23:11:46 +0700

    create change theme mode

--- 7_theme_clock/script.js
+++ 7_theme_clock/script.js
@@ -10,3 +10,15 @@ const time = document.querySelector('.time'),
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+modeToggle.addEventListener("click", (e) => {
+    const html = document.querySelector("html");
+    if (html.classList.contains("light"))    {
+        html.classList.remove("light");
+        e.target.innerHTML = "Light Mode";
+    }
+    else {
+        html.classList.add("light");
+        e.target.innerHTML = "Dark Mode";
+    }
+});
