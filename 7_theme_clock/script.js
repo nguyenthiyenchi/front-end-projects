82e150274689f9ab6e7e87132ff1970f0246ee80
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2024-03-05 21:22:48 +0700

    get values from html

---
+++ 7_theme_clock/script.js
@@ -0,0 +1,12 @@
+const modeToggle = document.querySelector(".mode");
+
+const hour = document.querySelector(".hour"),
+    minute = document.querySelector(".minute"),
+    second = document.querySelector(".second");
+
+
+const time = document.querySelector('.time'),
+    date = document.querySelector('.date');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
