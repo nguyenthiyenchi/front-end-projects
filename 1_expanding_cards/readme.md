e17ace221f199f3ab49e8d0c6dd4d41e7b95f613
ume <chi.nguyenthiyen.203@gmail.com>
2024-08-10 08:35:02 +0000

    update readme, "3_hidden_search" to "3_background"

--- 1_expanding_cards/readme.md
+++ 1_expanding_cards/readme.md
@@ -1,11 +1,11 @@
 ## Expanding Cards
-1. Finish Date: Friday, October 20, 2023
-2. Project Demo:
-![Expanding Cards](https:)
-3. Desccription: Developed an interactive card interface featuring five distinct cat images. 
+1. Project Demo:
+![Expanding Cards](./resources/cats.gif)
+
+2. Desccription: Developed an interactive card interface featuring five distinct cat images. 
     - When a user hovers over a card, the cat's name appears vertically aligned.
     - Upon clicking, the selected card expands to reveal the full image, enhancing visual engagement.
 
-4. Purpose: 
+3. Purpose: 
     - To present content in a visually appealing and interactive way. 
     - This allows users to engage with the information presented on the card by expanding and collapsing it with a click or hover action, enhancing user experience and engagement on a webpage.
---
+++ 3_background/bg.js
@@ -0,0 +1,17 @@
+var bg = '' +
+'   <div class="bg">' +
+'        <div class="cat">' +
+'            <i class="bx"><img src="./resources/catLeft.gif" style="width: 140px;"></i>' +
+'            <i class="bx"><img src="./resources/catLeft_.gif" style="width: 90px;"></i>' +
+'            <i class="bx"><img src="./resources/catRight.gif" style="width: 70px;"></i>' +
+'            <i class="bx"><img src="./resources/catRight_.gif" style="width: 85px;"></i>' +
+'            <i class="bx"><img src="./resources/catLeft.gif" style="width: 120px;"></i>' +
+'            <i class="bx"><img src="./resources/catLeft_.gif" style="width: 95px;"></i>' +
+'            <i class="bx"><img src="./resources/catRight.gif" style="width: 65px;"></i>' +
+'            <i class="bx"><img src="./resources/catRight_.gif" style="width: 100px;"></i>' +
+'            <i class="bx"><img src="./resources/catLeft.gif" style="width: 130px;"></i>' +
+'            <i class="bx"><img src="./resources/catLeft_.gif" style="width: 110px;"></i>' +
+'        </div>' +
+'    </div>' +
+'';
+document.writeln(bg);
